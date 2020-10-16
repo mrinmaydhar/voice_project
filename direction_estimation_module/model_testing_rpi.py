b99f0f44671a8970cc07302917fc24584f4e5e9c
SamsLogic <samygarg97@gmail.com>
2020-10-16 18:47:49 +0530

    Update model_testing_rpi.py

--- direction_estimation_module/model_testing_rpi.py
+++ direction_estimation_module/model_testing_rpi.py
@@ -72,28 +72,28 @@ while True:
             b = np.fromstring(data,dtype=np.int16)[1::4]
             c = np.fromstring(data,dtype=np.int16)[2::4]
             d = np.fromstring(data,dtype=np.int16)[3::4]
-            frames0.append(np.array(a,dtype=np.float16)/255)
-            frames1.append(np.array(b,dtype=np.float16)/255)
-            frames2.append(np.array(c,dtype=np.float16)/255)
-            frames3.append(np.array(d,dtype=np.float16)/255)
-        frames0 = np.array(frames0,dtype=np.float16)
-        frames1 = np.array(frames1,dtype=np.float16)
-        frames2 = np.array(frames2,dtype=np.float16)
-        frames3 = np.array(frames3,dtype=np.float16)
+            frames0.append(np.array(a,dtype=np.float32)/255)
+            frames1.append(np.array(b,dtype=np.float32)/255)
+            frames2.append(np.array(c,dtype=np.float32)/255)
+            frames3.append(np.array(d,dtype=np.float32)/255)
+        frames0 = np.array(frames0,dtype=np.float32)
+        frames1 = np.array(frames1,dtype=np.float32)
+        frames2 = np.array(frames2,dtype=np.float32)
+        frames3 = np.array(frames3,dtype=np.float32)
         frames0 = np.reshape(frames0,(184,256))
         frames1 = np.reshape(frames1,(184,256))
         frames2 = np.reshape(frames2,(184,256))
         frames3 = np.reshape(frames3,(184,256))
-        pred = model.predict(np.array([frames0,frames1,frames2,frames3],dtype=np.float16))
+        pred = model.predict(np.array([frames0,frames1,frames2,frames3],dtype=np.float32))
         print(pred)
-        if pred[0] > 0.88:
+        if np.sum(pred)/4 > 0.88:
             print("Hello")
             frames0_dir = np.reshape(frames0,(184,256,1))
             frames1_dir = np.reshape(frames1,(184,256,1))
             frames2_dir = np.reshape(frames2,(184,256,1))
             frames3_dir = np.reshape(frames3,(184,256,1))
             dir_frame = [frames0_dir,frames1_dir,frames2_dir,frames3_dir]
-            dir_frame = np.array(dir_frame,dtype=np.float16)
+            dir_frame = np.array(dir_frame,dtype=np.float32)
             dir_frame = np.reshape(dir_frame,(1,*dir_frame.shape))
             print(dir_frame.shape)
             
@@ -153,10 +153,10 @@ while True:
             df1 = {"name":name,"label":label}
             df1 = pd.DataFrame(df1)
             df1.to_csv('/home/pi/Project_V/voice_data_testing.csv',mode='a',index=False,header=False)
-            model.fit(np.array([frames0,frames1,frames2,frames3],dtype=np.float16),[lb,lb,lb,lb],batch_size=1,epochs=1)
+            model.fit(np.array([frames0,frames1,frames2,frames3],dtype=np.float32),[lb,lb,lb,lb],batch_size=1,epochs=1)
             model.save('/home/pi/Project_V/models/4th_version/voice_button_model_lstm.h5')
             if lb == 1:
-                model.fit(np.array([frames0,frames1,frames2,frames3],dtype=np.float16),[lb,lb,lb,lb],batch_size=1,epochs=1)
+                model.fit(np.array([frames0,frames1,frames2,frames3],dtype=np.float32),[lb,lb,lb,lb],batch_size=1,epochs=1)
                 model.save('/home/pi/Project_V/models/4th_version/voice_button_model_lstm.h5')
                 break
 	
