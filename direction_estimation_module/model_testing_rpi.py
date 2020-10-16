23118bb788857d05bf5870d7b0863b3a7624c381
SamsLogic <samygarg97@gmail.com>
2020-10-16 17:06:51 +0530

    Update model_testing_rpi.py

--- direction_estimation_module/model_testing_rpi.py
+++ direction_estimation_module/model_testing_rpi.py
@@ -72,19 +72,19 @@ while True:
             b = np.fromstring(data,dtype=np.int16)[1::4]
             c = np.fromstring(data,dtype=np.int16)[2::4]
             d = np.fromstring(data,dtype=np.int16)[3::4]
-            frames0.append(np.array(a,dtype=np.float16)/255)
-            frames1.append(np.array(b,dtype=np.float16)/255)
-            frames2.append(np.array(c,dtype=np.float16)/255)
-            frames3.append(np.array(d,dtype=np.float16)/255)
-        frames0 = np.array(frames0,dtype=np.float16)
-        frames1 = np.array(frames1,dtype=np.float16)
-        frames2 = np.array(frames2,dtype=np.float16)
-        frames3 = np.array(frames3,dtype=np.float16)
+            frames0.append(np.array(a,dtype=np.float32)/255)
+            frames1.append(np.array(b,dtype=np.float32)/255)
+            frames2.append(np.array(c,dtype=np.float32)/255)
+            frames3.append(np.array(d,dtype=np.float32)/255)
+        frames0 = np.array(frames0,dtype=np.float32)
+        frames1 = np.array(frames1,dtype=np.float32)
+        frames2 = np.array(frames2,dtype=np.float32)
+        frames3 = np.array(frames3,dtype=np.float32)
         frames0 = np.reshape(frames0,(184,256))
         frames1 = np.reshape(frames1,(184,256))
         frames2 = np.reshape(frames2,(184,256))
         frames3 = np.reshape(frames3,(184,256))
-        pred = model.predict([frames0,frames1,frames2,frames3])
+        pred = model.predict(np.array([frames0,frames1,frames2,frames3],dtype=np.float32))
         print(pred)
         if pred[0] > 0.88:
             print("Hello")
@@ -93,8 +93,10 @@ while True:
             frames2 = np.reshape(frames2,(184,256,1))
             frames3 = np.reshape(frames3,(184,256,1))
             dir_frame = [frames0,frames1,frames2,frames3]
-            dir_frame = np.array(dir_frame,dtype=np.float16)
+            dir_frame = np.array(dir_frame,dtype=np.float32)
+            dir_frame = np.reshape(dir_frame,(1,*dir_frame.shape))
             print(dir_frame.shape)
+            
             pred_dir = dir_model(dir_frame)
             print('direction: ',np.argmax(pred_dir))
             
